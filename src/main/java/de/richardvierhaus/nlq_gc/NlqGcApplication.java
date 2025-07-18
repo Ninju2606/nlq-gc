271c8733bc708d3dadb65e838ba61057b782d832
Richard Vierhaus <ri.vierhaus@web.de>
2025-07-18 11:40:22 +0200

    Initial Commit

---
+++ src/main/java/de/richardvierhaus/nlq_gc/NlqGcApplication.java
@@ -0,0 +1,13 @@
+package de.richardvierhaus.nlq_gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlqGcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlqGcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/richardvierhaus/nlq_gc/NlqGcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.richardvierhaus.nlq_gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlqGcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
